c5ed835cbb0546b2e9efa5a1020a58b61f43897c
JanBaig <janatbaig2@gmail.com>
2024-12-26 18:44:43 -0500

    updates

---
+++ swift-project/testCompile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_DIR="/Users/jan/Downloads/Dev/swift-project"
+
+MODE=$1
+TEST=$2
+
+pushd "${PROJECT_DIR}/build/${MODE}/swift-macosx-arm64"
+
+ninja -j 8 bin/swiftc
+
+# Return to the original directory
+popd
+
+python3 "${PROJECT_DIR}/llvm-project/llvm/utils/lit/lit.py" -sv --param swift_site_config="${PROJECT_DIR}/build/${MODE}/swift-macosx-arm64/test-macosx-arm64/lit.site.cfg" "${PROJECT_DIR}/${TEST}"
+
